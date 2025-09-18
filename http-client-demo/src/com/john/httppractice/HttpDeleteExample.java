0f9dbb916cdf0084f33808cc1e1bb7368281c06f
SonicDRJ <jhv9096@rit.edu>
2025-09-18 16:30:23 -0400

    Initial Commit
    Added initial commit with examples from Copilot.

---
+++ http-client-demo/src/com/john/httppractice/HttpDeleteExample.java
@@ -0,0 +1,20 @@
+package com.john.httppractice;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpDeleteExample {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body:\n" + response.body());
+    }
+}
---
+++ http-client-demo/src/com/john/httppractice/HttpGetExample.java
@@ -0,0 +1,22 @@
+package com.john.httppractice;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class HttpGetExample {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body:\n" + response.body());
+    }
+}
+
